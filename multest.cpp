a55358344ed80033d4236af486e0324c66504c1d
Reimar Döffinger <Reimar.Doeffinger@gmx.de>
2019-01-05 15:28:10 +0100

    Add some multiply/divide tests.

---
+++ multest.cpp
@@ -0,0 +1,24 @@
+unsigned short mul(unsigned short x, unsigned char t)
+{
+    return x * t;
+}
+
+unsigned short mul10(unsigned short x)
+{
+    return x * 10;
+}
+
+unsigned short div10(unsigned short x)
+{
+    return x / 10;
+}
+
+unsigned char cdiv10(unsigned char x)
+{
+    return x / 10;
+}
+
+unsigned char cdiv100(unsigned char x)
+{
+    return x / 100;
+}
